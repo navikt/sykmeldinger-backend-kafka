6e3fbe59e34697d8e676647613837e91b27a09a9
Joakim Taule Kartveit <joakim.taule.kartveit@nav.no>
2023-12-15 05:46:29 +0100

    Update build.gradle.kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -106,6 +106,9 @@ dependencies {
 tasks {
 
     shadowJar {
+mergeServiceFiles {
+     setPath("META-INF/services/org.flywaydb.core.extensibility.Plugin")
+ }
         archiveBaseName.set("app")
         archiveClassifier.set("")
         isZip64 = true
