e0bb1796c9fb1814c5f83ecc604f40a8f39b663a
Andreas Sagen Aspaas <danduras@gmail.com>
2024-03-01 08:13:09 +0100

    chore: remove behandlingsutfall kafka consumer

--- src/main/kotlin/no/nav/sykmeldinger/behandlingsutfall/Behandlingsutfall.kt
+++
@@ -1,9 +0,0 @@
-package no.nav.sykmeldinger.behandlingsutfall
-
-import no.nav.syfo.model.RuleInfo
-
-data class Behandlingsutfall(
-    val sykmeldingId: String,
-    val status: String,
-    val ruleHits: List<RuleInfo>,
-)
