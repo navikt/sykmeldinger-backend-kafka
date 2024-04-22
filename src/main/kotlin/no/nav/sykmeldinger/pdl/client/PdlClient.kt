388e5008861df9345c8225f1ccbba5bcf505297b
Jørn-Are Flaten <ja.flaten91@gmail.com>
2024-04-22 15:07:01 +0200

    chore: behandlingsnummer

--- src/main/kotlin/no/nav/sykmeldinger/pdl/client/PdlClient.kt
+++ src/main/kotlin/no/nav/sykmeldinger/pdl/client/PdlClient.kt
@@ -29,6 +29,7 @@ class PdlClient(
                 .post(basePath) {
                     setBody(getPersonRequest)
                     header(HttpHeaders.Authorization, "Bearer $token")
+                    header("Behandlingsnummer", "B229")
                     header(temaHeader, tema)
                     header(HttpHeaders.ContentType, "application/json")
                 }
