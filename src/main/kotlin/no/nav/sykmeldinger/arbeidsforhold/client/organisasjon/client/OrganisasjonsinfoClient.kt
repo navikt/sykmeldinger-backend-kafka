27fca50d5500a4c603458818eec48560c4c95057
Andreas Sagen Aspaas <danduras@gmail.com>
2025-06-21 11:41:10 +0200

    fix: handle 404 with exception

--- src/main/kotlin/no/nav/sykmeldinger/arbeidsforhold/client/organisasjon/client/OrganisasjonsinfoClient.kt
+++ src/main/kotlin/no/nav/sykmeldinger/arbeidsforhold/client/organisasjon/client/OrganisasjonsinfoClient.kt
@@ -2,6 +2,7 @@ package no.nav.sykmeldinger.arbeidsforhold.client.organisasjon.client
 
 import io.ktor.client.HttpClient
 import io.ktor.client.call.body
+import io.ktor.client.plugins.ClientRequestException
 import io.ktor.client.request.get
 import io.ktor.http.HttpStatusCode
 import no.nav.sykmeldinger.application.metrics.HTTP_CLIENT_HISTOGRAM
@@ -23,12 +24,15 @@ class OrganisasjonsinfoClient(
                 .startTimer()
         try {
             val response = httpClient.get("$url/api/v1/organisasjon/$orgNummer/noekkelinfo")
-            if (response.status == HttpStatusCode.NotFound) {
-                val errorResponse = response.body<OrganisjonsinfoErrorResponse>()
+            return response.body<Organisasjonsinfo>().navn.getNameAsString()
+        } catch (ex: ClientRequestException) {
+            if(ex.response.status == HttpStatusCode.NotFound) {
+                val errorResponse = ex.response.body<OrganisjonsinfoErrorResponse>()
                 throw OrganisasjonNotFoundException(errorResponse.melding)
             }
-            return response.body<Organisasjonsinfo>().navn.getNameAsString()
-        } catch (e: Exception) {
+            throw ex
+        }
+        catch (e: Exception) {
             log.error("Noe gikk galt ved henting av organisasjon $orgNummer fra ereg", e)
             throw e
         } finally {
